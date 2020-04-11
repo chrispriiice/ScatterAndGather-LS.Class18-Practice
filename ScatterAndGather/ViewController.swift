e055ad66771b742d2ee366117fe511efc2955e17
Chris Price <christopher.r.price@gmail.com>
2020-04-11 12:10:36 -0400

    Initial commit.

---
+++ ScatterAndGather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScatterAndGather
+//
+//  Created by Chris Price on 4/11/20.
+//  Copyright © 2020 com.chrispriiice. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
